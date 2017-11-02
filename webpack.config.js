3f592f1bb893a82cd69fc8f6c6d3bafbfb922616
Davidkov <Emil.Davidkov@experian.com>
2017-11-02 11:39:30 +0200

    removed separate css folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-	filename: './css/main.css'
+	filename: 'main.css'
 });
 
 module.exports = {
