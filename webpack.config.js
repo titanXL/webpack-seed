e0cb700321595824ec5f6bdc30a125028601493b
Davidkov <Emil.Davidkov@experian.com>
2017-11-02 11:25:26 +0200

    removed html minification

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
 			},
 			{
 				test: /\.html$/,
-				use: ['html-loader?interpolate']
+				use: ['html-loader?interpolate&minimize=false']
 			},
 			{
 				test: /\.(jpg|png|svg)$/,
